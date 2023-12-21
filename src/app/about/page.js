bfb587b79aa7a391617b38715dd134878fe63902
Jakob Douglas <jakobdouglas1@gmail.com>
2023-12-21 10:31:21 +1030

    docs: remove lorem, write about me section

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -21,49 +21,44 @@ function page() {
           <div className="pb-3">
             <h2 className="text-sm font-medium opacity-40">Who I am</h2>
             <p className="text-sm font-thin opacity-90">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
-              officiis corrupti est tenetur aut nisi porro ea odio ducimus
-              maxime!
+              My name is Jakob Douglas, I'm 28 years old from Adelaide, South
+              Australia and I enjoy learning how things work and then creating
+              amazing things with this knowledge
             </p>
           </div>
           <div className="pb-3">
             <h2 className="text-sm font-medium opacity-40">What I do</h2>
             <p className="text-sm font-thin opacity-90">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Doloremque, enim ad nisi facilis rerum eveniet eius ipsam, ratione
-              ex dicta ut itaque obcaecati voluptate quae impedit possimus
-              provident vero eligendi soluta nobis magni pariatur. Neque quia
-              asperiores est at, nisi tenetur reiciendis doloribus fuga delectus
-              quibusdam quis tempora perferendis voluptatem voluptate omnis,
-              architecto ratione sit dolorem tempore ipsa distinctio ut iste
-              expedita nemo. Illo commodi itaque, quod totam molestias aut
-              facere quae aperiam voluptatum veritatis sit obcaecati similique
-              nam beatae accusamus reiciendis vel laboriosam consequuntur
-              tempora ipsam fugiat. Neque maxime voluptas cupiditate tenetur
-              possimus voluptate distinctio magni ullam voluptatum libero?
+              Currently I work towards bettering my knowledge in the field of
+              software engineering and building projects, working on self
+              development skills and gaining new information within the
+              industry. I create projects and test my knowledge and design
+              process.
             </p>
           </div>
           <div className="pb-10">
             <h2 className="text-sm font-medium opacity-40">What I did</h2>
             <p className="text-sm font-thin opacity-90">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
-              illo unde vel harum est deleniti libero, quia iure, temporibus,
-              voluptatibus necessitatibus deserunt aliquam delectus. Nesciunt
-              quia quam iste itaque animi ut corrupti deleniti recusandae nam
-              voluptatem minus assumenda porro sed saepe dicta in, quos dolorem
-              doloribus voluptates! Natus, laboriosam in?
+              I have come from a varied background, from Hospitality and client
+              management, dealing with a variety of teams and liaising with
+              multiple organizations on and off sites to come together for a
+              specific goal things such as project management and team
+              development are not foreign to me. I have also had a vast history
+              in travel and tourism, building and contracted work. This Mixture
+              of backgrounds and workplaces has driven me to find new things and
+              be the best I can at them, which has now led me to the software
+              industry in which I am thoroughly enjoying and look forward to
+              elevating my skills and knowledge even further in.
             </p>
           </div>
           <div className="pb-10">
             <p className="text-sm font-thin opacity-90">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-              eaque doloremque voluptatibus
+              I look forward to the future of this field and working with some
+              amazing people
             </p>
           </div>
           <div>
-            <p className="text-sm font-thin opacity-90">
-              Lorem ipsum dolor sit
-            </p>
+            <p className="text-sm font-thin opacity-90">-sincerely</p>
           </div>
         </main>
         <aside className="mx-5 mt-3 flex w-1/2 flex-col gap-20">
