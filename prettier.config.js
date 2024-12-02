34efdd1a81eae08a599e937b79f6e53da199381e
Jakob Douglas <jakobdouglas1@gmail.com>
2024-12-02 19:15:02 +1030

    chore: install and configure eslint-config-prettier to prevent ESLint <-> Prettier conflicts

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,10 @@
-// prettier.config.js
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
 module.exports = {
-	plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none'
 };
