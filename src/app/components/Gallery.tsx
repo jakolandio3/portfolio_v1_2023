4283a51a2985a54bd1bb35c64dd805479143cae5
Jakob Douglas <jakobdouglas1@gmail.com>
2024-12-02 18:23:35 +1030

    PORTF-9 refactor: create Gallery component abstraction

---
+++ src/app/components/Gallery.tsx
@@ -0,0 +1,89 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+
+function Gallery({ items }: GalleryProps) {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [currentImage, setCurrentImage] = useState(1);
+
+  return (
+    <div className="grid-flow-rows grid gap-10 md:grid-cols-3 md:gap-5">
+      {items.map((item, index) => (
+        <div
+          onClick={() => {
+            setCurrentImage(index);
+            setOpen(true);
+          }}
+          className="flex w-full items-center gap-12 rounded-lg border hover:cursor-pointer md:flex-col md:gap-0  md:border-none"
+          key={item.title}
+        >
+          <h3 className="m-2 border-b-2 text-center text-xl font-semibold">
+            {item.title}
+          </h3>
+          {loading === true ? (
+            <div className="relative h-full w-full">
+              <div className=" absolute flex h-full w-full content-center items-center justify-center self-center bg-black">
+                <span className="loading loading-bars loading-lg"></span>
+              </div>{" "}
+              <Image
+                onLoad={() => {
+                  setLoading(false);
+                }}
+                className="self-center"
+                alt={item.title}
+                width={400}
+                height={600}
+                src={item.imageUrl}
+              />{" "}
+            </div>
+          ) : (
+            <Image
+              className="self-center"
+              alt={item.title}
+              width={400}
+              height={600}
+              src={item.imageUrl}
+            />
+          )}
+        </div>
+      ))}
+      <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        plugins={[Zoom]}
+        slides={items.map((item) => buildLightBoxSlide(item))}
+        index={currentImage}
+      />
+    </div>
+  );
+}
+
+function buildLightBoxSlide(image: GalleryItem) {
+  return {
+    src: image.imageUrl,
+    width: 3840,
+    height: 2560,
+    srcSet: [
+      { src: image.imageUrl, width: 320, height: 213 },
+      { src: image.imageUrl, width: 640, height: 426 },
+      { src: image.imageUrl, width: 1200, height: 800 },
+      { src: image.imageUrl, width: 2048, height: 1365 },
+      { src: image.imageUrl, width: 3840, height: 2560 },
+    ],
+  };
+}
+
+export default Gallery;
+
+interface GalleryProps {
+  items: GalleryItem[];
+}
+
+export interface GalleryItem {
+  imageUrl: string;
+  type: string;
+  title: string;
+}
--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,34 +1,24 @@
-"use client";
 import Button from "@/app/components/Button";
+import Gallery from "@/app/components/Gallery";
 import PageLayout from "@/app/components/PageLayout";
-import List from "@/app/library/AppList";
-import Image from "next/image";
+import { AppList, AppView } from "@/app/library/AppList";
 import Link from "next/link";
-import { useState } from "react";
+import { notFound } from "next/navigation";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { SiGithub } from "react-icons/si";
-import Lightbox from "yet-another-react-lightbox";
-import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/styles.css";
 
-function Page({ params }) {
-  const [open, setOpen] = useState(false);
-  const [loadingImage, setLoadingImage] = useState(true);
-  const [currentImage, setCurrentImage] = useState(1);
-  let app = List.filter((app) => app.name === params.slug).at(0);
+async function fetchAppData(name: string) {
+  const app = AppList.find((app) => app.name === name);
+  return app;
+}
 
-  const imageArray = app.gallery.map((image) => ({
-    src: image.imageUrl,
-    width: 3840,
-    height: 2560,
-    srcSet: [
-      { src: image.imageUrl, width: 320, height: 213 },
-      { src: image.imageUrl, width: 640, height: 426 },
-      { src: image.imageUrl, width: 1200, height: 800 },
-      { src: image.imageUrl, width: 2048, height: 1365 },
-      { src: image.imageUrl, width: 3840, height: 2560 },
-    ],
-  }));
+async function Page({ params }: { params: { slug: string } }) {
+  const app = await fetchAppData(params.slug);
+
+  if (!app) {
+    notFound();
+  }
 
   return (
     <PageLayout
@@ -36,63 +26,14 @@ function Page({ params }) {
       title={app.name}
       blurb={app.description}
     >
-      <div className="grid-flow-rows grid gap-10 md:grid-cols-3 md:gap-5">
-        {app.gallery.map((item, index) => (
-          <div
-            onClick={() => {
-              setCurrentImage(index);
-              setOpen(true);
-            }}
-            className="flex w-full items-center gap-12 rounded-lg border hover:cursor-pointer md:flex-col md:gap-0  md:border-none"
-            key={item.title}
-          >
-            <h3 className="m-2 border-b-2 text-center text-xl font-semibold">
-              {item.title}
-            </h3>
-            {loadingImage === true ? (
-              <div className="relative h-full w-full">
-                <div className=" absolute flex h-full w-full content-center items-center justify-center self-center bg-black">
-                  <span className="loading loading-bars loading-lg"></span>
-                </div>{" "}
-                <Image
-                  onLoad={() => {
-                    setLoadingImage(false);
-                  }}
-                  className="self-center"
-                  alt={item.title}
-                  width={400}
-                  height={600}
-                  src={item.imageUrl}
-                />{" "}
-              </div>
-            ) : (
-              <Image
-                className="self-center"
-                alt={item.title}
-                width={400}
-                height={600}
-                src={item.imageUrl}
-              />
-            )}
-          </div>
-        ))}
-        <Lightbox
-          // on={{ view: ({ index: currentIndex }) => setImage(currentIndex) }}
-          open={open}
-          close={() => setOpen(false)}
-          plugins={[Zoom]}
-          showPrevNext={true}
-          slides={imageArray}
-          index={currentImage}
-        />
-      </div>
+      <Gallery items={app.images} />
     </PageLayout>
   );
 }
 
 export default Page;
 
-function AppExtLinks({ app }) {
+function AppExtLinks({ app }: { app: AppView }) {
   return (
     <div className="mt-12 flex flex-col md:mt-0">
       {" "}
@@ -116,7 +57,7 @@ function AppExtLinks({ app }) {
           </Link>
         </div>
         <span className=" text-lg font-extrabold">
-          <Button scroll={false} to="/projects">
+          <Button scroll={false} href="/projects">
             &larr; Back to projects
           </Button>
         </span>
